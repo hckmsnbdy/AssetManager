caddd720edc36d0d2e357999dcf3fabfa0ecd51e
Omer Faruk Akcam <hckmsnbdy@gmail.com>
2025-10-28 11:00:27 -0400

    Initial:   -Added:     -Asses as a super class     -House and Vehicle as a child class     -Their constructors, getters and setters.

---
+++ src/main/java/com/pluralsight/House.java
@@ -0,0 +1,64 @@
+package com.pluralsight;
+
+public class House extends Asset{
+    String address;
+    int condition ; // (1 -excellent, 2 -good, 3 -fair, 4 -poor)
+    int squareFoot;
+    int lotSize;
+
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public int getCondition() {
+        return condition;
+    }
+    public void setCondition(int condition) {
+        this.condition = condition;
+    }
+    public int getSquareFoot() {
+        return squareFoot;
+    }
+    public void setSquareFoot(int squareFoot) {this.squareFoot = squareFoot;
+    }
+    public int getLotSize() {
+        return lotSize;
+    }
+    public void setLotSize(int lotSize) {
+        this.lotSize = lotSize;
+    }
+    public House(String description,
+                 String dateAcquired,
+                 double originalCost,
+                 String address,
+                 int condition,
+                 int squareFoot,
+                 int lotSize) {
+        super(description, dateAcquired, originalCost);
+        this.address = address;
+        this.condition = condition;
+        this.squareFoot = squareFoot;
+        this.lotSize = lotSize;
+    }
+    public House(String description,
+                 String dateAcquired,
+                 double originalCost) {
+        super(description, dateAcquired, originalCost);
+    }
+
+    @Override
+    public double getValue() {
+        // A house's value is determined as
+        // $180.00 per square foot (excellent)
+        // $130.00 per square foot (good)
+        // $90.00 per square foot (fair)
+        // $80.00 per square foot (poor)
+        // PLUS 25 cents per square foot of lot size
+        return super.getValue();
+    }
+
+    //    public double getValue
+
+}
---
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -0,0 +1,7 @@
+package com.pluralsight;
+
+public class Vehicle extends Asset{
+    public Vehicle(String description, String dateAcquired, double originalCost) {
+        super(description, dateAcquired, originalCost);
+    }
+}
